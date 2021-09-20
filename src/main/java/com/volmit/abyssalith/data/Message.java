20f435471b33a313dfc2dd48129e1e6221a1ed8c
cyberpwn <danielmillst@gmail.com>
2021-09-20 00:49:44 -0400

    Fix data ignore

---
+++ src/main/java/com/volmit/abyssalith/data/Message.java
@@ -0,0 +1,31 @@
+/*
+ * Abyssalith is a Discord Bot for Volmit Software's Community
+ * Copyright (c) 2021 VolmitSoftware (Arcane Arts)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.volmit.abyssalith.data;
+
+import com.volmit.abyssalith.io.DataType;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true, fluent = true)
+public class Message implements DataType {
+    private long id = 0;
+    private double experience = 0.0f;// soon to implement (most used chats etc...)
+
+}
---
+++ src/main/java/com/volmit/abyssalith/data/User.java
@@ -0,0 +1,47 @@
+/*
+ * Abyssalith is a Discord Bot for Volmit Software's Community
+ * Copyright (c) 2021 VolmitSoftware (Arcane Arts)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.volmit.abyssalith.data;
+
+import com.volmit.abyssalith.io.DataType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Accessors(chain = true, fluent = true)
+public class User implements DataType {
+    private long id = 0;
+    private double experience = 0.0f;
+    private double money = 0.00f;
+
+    //private UserStatistics stats = new UserStatistics();
+    private long messagesSent = 0;
+    private long reactions = 0;
+    private Set<String> roleIds = new HashSet<>();
+
+}
