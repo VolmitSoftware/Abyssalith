d1f54d7151a7cb6025be61fb2f50711a7ba69e92
cyberpwn <danielmillst@gmail.com>
2021-09-20 00:12:21 -0400

    Deploy script WIP

---
+++ deploy.bat
@@ -0,0 +1,20 @@
+@echo off
+set BUILD=0
+
+(
+set /p BUILD=
+)<build.number
+
+set /A BUILD=%BUILD%+1
+
+echo Build #%BUILD%
+
+(
+echo %build%
+)>build.number
+start /wait gradlew clean shadowJar
+echo F|xcopy /y /s /f /q "build/libs/Abyssalith-1.0-all.jar" "staging/Abyssalith.jar"
+docker build . -t arcane/abyssalith:b%BUILD%
+doctl registry login
+docker tag arcane/abyssalith:b%BUILD% registry.digitalocean.com/cargo-dock/arcane/abyssalith:b%BUILD%
+docker push registry.digitalocean.com/cargo-dock/arcane/abyssalith:b%BUILD%
