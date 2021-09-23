e6c0ce8ac93e7c60390b57ec87391fae57d5ac82
Brian Fopiano <mypsn1550@gmail.com>
2021-09-22 20:38:22 -0700

    .

--- src/main/java/com/volmit/abyssalith/commands/rroles/LanguageRoles.java
+++ src/main/java/com/volmit/abyssalith/commands/rroles/LanguageRoles.java
@@ -45,17 +45,6 @@ public class LanguageRoles extends VolmitCommand {
     public void handle(List<String> args, GuildMessageReceivedEvent e) {
         i("Mention Roles List Initialized"); //TODO ACTUALLY MAKE A LANGUAGE SYSTEM
         VolmitEmbed embed = new VolmitEmbed(" REACTION ROLES!!", e.getMessage());
-        User u = Main.getLoader().getUser(e.getAuthor().getIdLong());
-        embed.setDescription("*This should explain how to use the reaction roles system, should you have any questions!*");
-        embed.addField("Create a Reaction-able Message!", " First of all your message needs to contain the following: `"
-                + Kit.get().ReactionRoleString
-                + "`, anywhere in the message, AND you need to be able to view audit log (for now)", false);
-        embed.addField("Example: ", "" +
-                " - " + Kit.get().ReactionRoleString + " - \n"
-                + ":coin: for @role1 or :eyes: @role2 even :hearts: for @role3\n"
-                + "*__Supports Server Emotes__*", false);
-        embed.addField("Adding the reactions!", "Finally the author must react to the message with your reactions (*in order of mentioned roles*) to assign them eg: [:coin: :eyes: :hearts:] ", false);
-        embed.addField("*Notes*", "*This will remove any reactions that are NOT from the Message Author (prevents spam) Unless the rolemaster adds TOO MANY roles, it will clear the reactions for you to react once more*", false);
         embed.send(e.getMessage(), true, 1000);
 
     }
