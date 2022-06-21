60ffaf69b0da5798efdc79bf9433628feaf3259f
Sjoerd van de Goor <sjoerdvdgoor@hotmail.com>
2022-06-21 21:50:45 +0200

    Interpunction

--- src/main/java/com/volmit/abyssalith/commands/general/Chunky.java
+++ src/main/java/com/volmit/abyssalith/commands/general/Chunky.java
@@ -24,10 +24,10 @@ public class Chunky extends VolmitCommand {
                 e.getMessage()
         );
         em.setDescription("""
-                Iris comes with a built-in world generator(Using the core generation code from Chunky).
+                Iris comes with a built-in world generator (Using the core generation code from Chunky).
                 The generator that we feature is designed to replace Chunky in Iris worlds.
                 You can of course still use Chunky on vanilla worlds, as it may perform better there.
-                However, Using Chunky in an iris world may cause problems, Chunky is not at fault for this, 
+                However, Using Chunky in an iris world may cause problems, Chunky is not at fault for this,
                 but this is why we suggest not using it for iris worlds, as its already implemented as needed.
                 Plus, Iris' pregenerator makes use of the MCA world file system that Minecraft uses,
                 and writes directly to those files so the worlds can be generated much more quickly.
